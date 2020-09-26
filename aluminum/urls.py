74aee9ee6148b02262eedda44046d433924b662a
peteco <ebermania@gmail.com>
2020-09-26 20:44:30 -0300

    Extruders, systems and profiles list created.

--- aluminum/urls.py
+++ aluminum/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = 'aluminum'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.ExtruderListView.as_view(), name='index'),
+    path('<int:pk>/systems/', views.ProductSystemListView.as_view(), name='systems'),
+    path('<int:pk>/profiles/', views.ProfileListView.as_view(), name='profiles'),
+    path('<int:pk>/detail/', views.ProfileDetailView.as_view(), name='detail'),
 ]
--- aluminum/views.py
+++ aluminum/views.py
@@ -1,10 +1,36 @@
 from django.http import HttpResponse
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
-from .models import Profile
+from .models import Extruder, ProductSystem, Profile
 
-class IndexView(generic.ListView):
+class ExtruderListView(generic.ListView):
     template_name = 'aluminum/index.html'
-    context_object_name = 'profiles_list'
+    context_object_name = 'extruders_list'
+
     def get_queryset(self):
-        return Profile.objects.order_by('-profile_code')[:20]
+        return Extruder.objects.order_by('-extruder_name')[:20]
+
+
+class ProductSystemListView(generic.ListView):
+    model = ProductSystem
+    template_name = 'aluminum/systems.html'
+    context_object_name = 'systems_list'
+
+    def get_queryset(self):
+        return ProductSystem.objects.filter(extruder__pk=self.kwargs['pk'])
+
+
+class ProfileListView(generic.ListView):
+    model = Profile
+    template_name = 'aluminum/profiles.html'
+    context_object_name = 'profiles_list'
+
+    def get_queryset(self):
+        return Profile.objects.filter(system__pk=self.kwargs['pk'])
+
+
+class ProfileDetailView(generic.DetailView):
+    model = Profile
+    template_name = 'aluminum/detail.html'
+    context_object_name = 'profile'
