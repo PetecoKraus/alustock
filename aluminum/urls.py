b570bc407d6d742f2ca37c1c4d13084588556bc9
peteco <ebermania@gmail.com>
2020-09-27 20:02:23 -0300

    Images showing fixed

--- aluminum/urls.py
+++ aluminum/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('<int:pk>/systems/', views.ProductSystemListView.as_view(), name='systems'),
     path('<int:pk>/profiles/', views.ProfileListView.as_view(), name='profiles'),
     path('<int:pk>/detail/', views.ProfileDetailView.as_view(), name='detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- alustock/settings.py
+++ alustock/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,4 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    "/home/peteco/Documentos/Proyectos/AluStock/alustock/aluminum/static/",
+]
